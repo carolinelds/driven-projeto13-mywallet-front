4300a515b48654e6cd31970c02e1bf8a93384c06
Caroline LS <carolinelds@gmail.com>
2022-05-09 07:11:51 -0300

    Add sign-in page and process

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import "./css/reset.css";
+import styled from "styled-components";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import SignInPage from "./components/SignInPage";
+import TokenContext from "./contexts/TokenContext";
+
 export default function App() {
+
+    const [token, setToken] = useState("");
+
     return (
-        <>
-            <p>TESTE</p>
-        </>
+        <Div>
+            <TokenContext.Provider value={{ token, setToken }}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<SignInPage />}></Route>
+                    </Routes>
+                </BrowserRouter>
+            </TokenContext.Provider>
+        </Div>
     );
 }
+
+const Div = styled.div`
+    background-color: #8C11BE;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    * {
+        font-family: 'Raleway';
+        box-sizing: border-box;
+    }
+`;
---
+++ src/components/SignInPage.js
@@ -0,0 +1,114 @@
+import styled from "styled-components";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useContext, useState } from "react";
+import TokenContext from "../contexts/TokenContext.js";
+
+export default function SignInPage() {
+
+    const { setToken } = useContext(TokenContext);
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    async function login(event){
+        event.preventDefault();
+
+        const body = {
+            email,
+            password
+        };
+
+        try {
+            const response = await axios.post("http://localhost:5000/sign-in", body);
+            console.log(response);
+
+            const newToken = response.data;
+            setToken(newToken);
+
+            nextPage();
+
+        } catch(e) {
+            window.alert("Usuário ou senha inválido(s), tente novamente.");
+            console.log(e);
+        }
+    };
+
+    let navigate = useNavigate();
+
+    function nextPage(){
+        navigate("/wallet");
+    }
+
+    return (
+        <Div>
+            <h1>MyWallet</h1>
+            <form onSubmit={login}>
+                <input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="E-mail" required />
+                <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Senha" required />
+                <button type="submit">Entrar</button>
+            </form>
+            <Link to="/sign-up">
+                <p>Primeira vez? Cadastre-se!</p>
+            </Link>
+        </Div>
+    )
+}
+
+const Div = styled.div`
+    max-width: 375px; /*Iphone 8*/
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100vh;
+    
+    h1 {
+        margin: 159px 0 24px 0;
+        font-family: 'Saira Stencil One';
+        font-weight: 400;
+        font-size: 32px;
+        line-height: 50px;
+        color: #FFFFFF;
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    input {
+        width: 326px;
+        height: 58px;
+        background: #FFFFFF;
+        border-radius: 5px;
+        font-weight: 400;
+        font-size: 20px;
+        line-height: 23px;
+        color: #000000;
+        padding: 15px;
+        margin-bottom: 13px;
+        border-style: none;
+    }
+
+    button {
+        margin-bottom: 36px;
+        width: 326px;
+        height: 46px;
+        background: #A328D6;
+        border-radius: 5px;
+        font-weight: 700;
+        font-size: 20px;
+        line-height: 23px;
+        color: #FFFFFF;
+        border-style: none;
+    }
+
+    p {
+        font-weight: 700;
+        font-size: 15px;
+        line-height: 18px;
+        color: #FFFFFF;
+    }
+`;
